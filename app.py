492e6e7166b1ac5865050bee59ec605438f994d6
Underc0de <38077842+underc0delabs@users.noreply.github.com>
2018-04-09 11:59:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def help(bot, update):
         print(update.message.from_user.get_profile_photos().photos)
         print(update.message.from_user.get_profile_photos().photos[0])
         print(update.message.from_user.get_profile_photos().photos[0].file_id)
+        print(update.message.from_user.get_profile_photos().photos[0][0].file_id)
     else:
         print('none')
 
