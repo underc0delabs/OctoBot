bf0837338e428d1e6d99e05c00cd3f4ee2c8190b
Underc0de <38077842+underc0delabs@users.noreply.github.com>
2018-04-09 10:23:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,8 @@ def start(bot, update):
 
 def help(bot, update):
     update.message.reply_text('La ayuda es para los debiles.')
+    update.message.reply_text(update.message.UserProfilePhotos.photos)
+    update.message.reply_text('No te la daré.')
 
 def echo(bot, update):
     if update.message.chat.type=='group':
